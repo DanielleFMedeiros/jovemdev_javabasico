a030e3a8fa50a827a0e00dec8436fa6990b47a10
DanielleFMedeiros <danielle_fariass@hotmail.com>
2023-05-22 11:38:17 -0300

    primeiro commit

--- src/main/java/primeira_aula/Main.java
+++ src/main/java/primeira_aula/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 		int vetor[] = new int[10];
 		for(int i = 0; i < 10; i++) {
-			String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+			String nome = JOptionPane.showInputDialog("Digited seu nome: ");
 			System.out.println(nome);
 			String sexo = JOptionPane.showInputDialog("Digite o sexo(F ou M): ");
 			System.out.println(sexo);
